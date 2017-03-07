6336c16ce4afa62467c3f6f6b125caef8dba5e45
JayManday <jmandel1027@gmail.com>
2017-03-07 15:30:29 -0500

    beginning to define vertex params

--- dijkstra/vertex.py
+++ dijkstra/vertex.py
@@ -6,4 +6,7 @@ class Vertex:
 		self.adjacent = { }
 	
 	def __str__(self):
-		
+		return str(self.id) + ' adjacent: ' + str([x.id for x in self.adjacent])
+	
+	def add_neighbor(self, neighbor, weight=0):
+		self.adjacent[neighbor] = weight
