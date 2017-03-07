a7eedcc7122480375c1b39fc76e23dd09ec17371
JayManday <jmandel1027@gmail.com>
2017-03-07 15:52:45 -0500

    mapping out heap w pseudo

--- dijkstra/vertex.py
+++ dijkstra/vertex.py
@@ -10,3 +10,16 @@ class Vertex:
 	
 	def add_neighbor(self, neighbor, weight=0):
 		self.adjacent[neighbor] = weight
+		
+	'''
+		create a priority queue/ heap to manage vertex progressions
+		
+		key = id aka node
+		adjacent = neighbors
+		key h[] = the number of neigbors with the key of id/node
+		heap array h[] with be inside of heap obj
+		
+		insert(node):
+			h[++neighbors] = node
+			siftUp( node) number of adjacent nodes in heap
+	'''
